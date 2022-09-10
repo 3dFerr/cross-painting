7e18510318c6b3228df9a6c294fb439fa95d91df
pauloferrti <pauloferrti@gmail.com>
2022-09-10 10:01:20 -0300

    04 - create the draw function

--- cross-painting.py
+++ cross-painting.py
@@ -1,8 +1,20 @@
-# sketch to run in Processing IDE - mode: Python
-# go to https://py.processing.org/tutorials/gettingstarted/ 
-# for setup processing python enviroment
+''' 
+sketch to run in Processing IDE - mode: Python
+go to https://py.processing.org/tutorials/gettingstarted/ 
+for setup processing python enviroment
+'''
 
 def setup():
   size(600,300) # you can change the size of window
   background(0) # color background set to black. choose you favorite color
 
+def draw():
+    # Colors can be changed. Choose the colors you want
+    if mousePressed:
+        fill(203,112,255)
+    else:
+        fill(255)
+            
+    rectMode(CENTER)
+    rect(mouseX, mouseY, 80, 20)
+    rect(mouseX, mouseY, 20, 80)
